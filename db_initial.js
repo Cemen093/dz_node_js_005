5d087b35b4da58711200ded5514ce2d1b033918d
Cemen093 <71636411+Cemen093@users.noreply.github.com>
2022-05-18 06:42:42 +0300

    fix

--- db_initial.js
+++ db_initial.js
@@ -1,32 +1,34 @@
-const express = require('express');
-const app = express();
 const MongoClient = require('mongodb').MongoClient;
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-var ObjectID = require('mongodb').ObjectId;
-var db;
+const dbName = "my_db";
 
 MongoClient.connect('mongodb://localhost:27017', function (err, client) {
     if (err){
         return console.log(err);
     }
-    db = client.db('my_bd');
-    app.listen(port, hostname, () => {
-        console.log(`Server running at http://${hostname}:${port}/`);
-    });
-})
+    const db = client.db(dbName);
 
-db.collection('manufacturer').insertMany([{name: "manufacturer_1"}, {name: "manufacturer_2"}, {name: "manufacturer_3"}])
-db.collection('category').insertMany([{name: "food"}, {name: "electronics"}, {name: "tool"},{name: "furniture"}])
-db.collection('products').insertMany([
-    {name: "apple", id_manufacturer: 1, id_category: 1},
-    {name: "banana", id_manufacturer: 1, id_category: 1},
-    {name: "hammer", id_manufacturer: 2, id_category: 3},
-    {name: "hammer", id_manufacturer: 2, id_category: 3},
-    {name: "iPhone", id_manufacturer: 3, id_category: 2},
-    {name: "nokia 3310", id_manufacturer: 3, id_category: 2},
-    {name: "table", id_manufacturer: 4, id_category: 4},
-    {name: "bed", id_manufacturer: 4, id_category: 4}
-])
+    db.collection('manufacturer').insertMany(
+        [{name: "manufacturer_1"},
+            {name: "manufacturer_2"},
+            {name: "manufacturer_3"}]
+    ).then(r => client.close());
+
+    db.collection('category').insertMany(
+        [{name: "food"},
+            {name: "electronics"},
+            {name: "tool"},
+            {name: "furniture"}]
+    ).then(r => client.close());
+
+    db.collection('products').insertMany([
+        {name: "apple", id_manufacturer: 1, id_category: 1},
+        {name: "banana", id_manufacturer: 1, id_category: 1},
+        {name: "hammer", id_manufacturer: 2, id_category: 3},
+        {name: "hammer", id_manufacturer: 2, id_category: 3},
+        {name: "iPhone", id_manufacturer: 3, id_category: 2},
+        {name: "nokia 3310", id_manufacturer: 3, id_category: 2},
+        {name: "table", id_manufacturer: 4, id_category: 4},
+        {name: "bed", id_manufacturer: 4, id_category: 4}
+    ]).then(r => client.close());
+})
--- server.js
+++ server.js
@@ -12,25 +12,150 @@ const port = 3000;
 
 var ObjectID = require('mongodb').ObjectId;
 var db;
+var _client;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 MongoClient.connect('mongodb://localhost:27017', function (err, client) {
     if (err){
         return console.log(err);
     }
     db = client.db('my_bd');
+    _client = client;
     app.listen(port, hostname, () => {
         console.log(`Server running at http://${hostname}:${port}/`);
     });
 })
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-
-
 app.get('/', function (req, res) {
-    res.send('Hello API');
+    res.send(`
+    <p>Hello API</p>
+    <p><a href="http://${hostname}:${port}/manufacturer">manufacturer</a></p>
+    <p><a href="http://${hostname}:${port}/category">category</a></p>
+    <p><a href="http://${hostname}:${port}/products">products</a></p>
+    `);
 })
+
+app.get('/manufacturer', function (req, res) {
+    db.collection('manufacturer').find().toArray(function (err, docs) {
+        if (err){
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.send(docs);
+    });
+})
+
+app.get('/manufacturer/:id', function (req, res) {
+    db.collection('manufacturer').findOne({ _id: ObjectID(req.params.id)}, function (err, doc) {
+        if (err){
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.send(doc);
+    });
+})
+
+app.post('/manufacturer', function (req, res) {
+    let product = {
+        name: req.body.name,
+    }
+    db.collection('manufacturer').insert(product, function (err, result) {
+        if (err){
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.send(product);
+    })
+})
+
+app.put('/manufacturer/:id', function (req, res) {
+    db.collection('manufacturer').updateOne(
+        {_id: ObjectID(req.params.id)},
+        {$set: {name: req.body.name}},
+        function (err, result) {
+            if (err){
+                console.log(err);
+                return res.sendStatus(500);
+            }
+            res.sendStatus(200);
+        }
+    )
+})
+
+app.delete('/manufacturer/:id', function (req, res) {
+    db.collection('manufacturer').deleteOne(
+        { _id: ObjectID(req.params.id) },
+        function (err, result) {
+            if (err){
+                console.log(err);
+                return res.sendStatus(500);
+            }
+            res.sendStatus(200);
+        }
+    )
+});
+
+app.get('/category', function (req, res) {
+    db.collection('category').find().toArray(function (err, docs) {
+        if (err){
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.send(docs);
+    });
+})
+
+app.get('/category/:id', function (req, res) {
+    db.collection('category').findOne({ _id: ObjectID(req.params.id)}, function (err, doc) {
+        if (err){
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.send(doc);
+    });
+})
+
+app.post('/category', function (req, res) {
+    let product = {
+        name: req.body.name,
+    }
+    db.collection('category').insert(product, function (err, result) {
+        if (err){
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.send(product);
+    })
+})
+
+app.put('/category/:id', function (req, res) {
+    db.collection('category').updateOne(
+        {_id: ObjectID(req.params.id)},
+        {$set: {name: req.body.name}},
+        function (err, result) {
+            if (err){
+                console.log(err);
+                return res.sendStatus(500);
+            }
+            res.sendStatus(200);
+        }
+    )
+})
+
+app.delete('/category/:id', function (req, res) {
+    db.collection('category').deleteOne(
+        { _id: ObjectID(req.params.id) },
+        function (err, result) {
+            if (err){
+                console.log(err);
+                return res.sendStatus(500);
+            }
+            res.sendStatus(200);
+        }
+    )
+});
 
 app.get('/products', function (req, res) {
     db.collection('products').find().toArray(function (err, docs) {
@@ -39,7 +164,7 @@ app.get('/products', function (req, res) {
             return res.sendStatus(500);
         }
         res.send(docs);
-    })
+    });
 })
 
 app.get('/products/:id', function (req, res) {
@@ -71,8 +196,8 @@ app.put('/products/:id', function (req, res) {
     db.collection('products').updateOne(
         {_id: ObjectID(req.params.id)},
         {$set: {name: req.body.name,
-            id_manufacturer: req.body.id_manufacturer,
-            id_category: req.body.id_category}},
+                id_manufacturer: req.body.id_manufacturer,
+                id_category: req.body.id_category}},
         function (err, result) {
             if (err){
                 console.log(err);
@@ -95,3 +220,7 @@ app.delete('/products/:id', function (req, res) {
         }
     )
 });
+
+app.get('/stop', function (req, res) {
+    _client.close();
+})
